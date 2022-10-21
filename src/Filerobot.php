41404c590d9f21a70ac841dd98774749313d3de3
bavuong0810 <bavuong0810@gmail.com>
2022-10-21 14:13:15 +0700

    update config FR, add more field to table and remove field when uninstall plugin

--- src/Filerobot.php
+++ src/Filerobot.php
@@ -1,10 +1,11 @@
 <?php declare(strict_types=1);
 
-namespace CloudImage;
+namespace Filerobot;
 
 use Shopware\Core\Framework\Plugin;
 use Shopware\Core\Framework\Plugin\Context\InstallContext;
 use Shopware\Core\Framework\Plugin\Context\ActivateContext;
+use Shopware\Core\Framework\Plugin\Context\UninstallContext;
 use Shopware\Core\System\SystemConfig\SystemConfigService;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use CloudImage\Component\DependencyInjection\CustomProfilerExtensions;
@@ -28,6 +29,29 @@ class Filerobot extends Plugin
         $config->set('Filerobot.config.frUploadDirectory', "/wp_assets");
     }
 
+    public function uninstall(UninstallContext $uninstallContext): void
+    {
+        if ($uninstallContext->keepUserData()) {
+            return;
+        }
+
+        $connection = \Shopware\Core\Kernel::getConnection();
+        $query = $connection->executeStatement("SELECT *
+                    FROM media 
+                    WHERE is_filerobot = 1");
+        $media = $query->fetchAllAssociative();
+        foreach ($media as $item) {
+            $media_id = $item['id'];
+            $connection->executeStatement('DELETE FROM `media_tag` where media_id = "' . $media_id . '"');
+            $connection->executeStatement('DELETE FROM `media_thumbnail` where media_id = "' . $media_id . '"');
+            $connection->executeStatement('DELETE FROM `media_translation` where media_id = "' . $media_id . '"');
+        }
+        $connection->executeStatement('DELETE FROM `media` where is_filerobot = 1');
+        $connection->executeStatement("ALTER TABLE `shopware`.`media` 
+        DROP COLUMN `is_filerobot`,
+        DROP COLUMN `url`;");
+    }
+
     public function activate(ActivateContext $activateContext): void
     {
         parent::activate($activateContext);
@@ -37,5 +61,10 @@ class Filerobot extends Plugin
         //set the specified values as defaults
         $config->set('Filerobot.config.frActivation', true);
         $config->set('Filerobot.config.frUploadDirectory', "/wp_assets");
+
+        $connection = \Shopware\Core\Kernel::getConnection();
+        $connection->executeStatement("ALTER TABLE `media` 
+        ADD COLUMN `url` VARCHAR(255) NULL AFTER `updated_at`,
+        ADD COLUMN `is_filerobot` TINYINT(1) NULL AFTER `url`;");
     }
 }
