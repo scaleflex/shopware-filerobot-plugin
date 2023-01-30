db742078e3910fb3d5989eb0548b5e7eedbdab20
Vương Đỗ <bavuong0810@gmail.com>
2023-01-30 23:03:40 +0700

    update migration

--- src/Migration/Migration1675060708FilerobotMedia.php
+++ src/Migration/Migration1675060708FilerobotMedia.php
@@ -23,9 +23,7 @@ class Migration1675060708FilerobotMedia extends MigrationStep
             `url` VARCHAR(255) NULL,
             `created_at` DATETIME(3) NOT NULL,
             `updated_at` DATETIME(3) NULL,
-            PRIMARY KEY (`id`),
-            KEY `fk.filerobot_media.media_id` (`media_id`),
-            CONSTRAINT `fk.filerobot_media.media_id` FOREIGN KEY (`media_id`) REFERENCES `media` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE
+            PRIMARY KEY (`id`)
         ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
         SQL;
         $connection->executeStatement($sql);
