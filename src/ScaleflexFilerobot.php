41a39202c5864a4c44f08ac7487a5a5fd7237c79
Vương Đỗ <bavuong0810@gmail.com>
2023-01-31 21:12:52 +0700

    refactor ScaleflexFilerobot

--- src/ScaleflexFilerobot.php
+++ src/ScaleflexFilerobot.php
@@ -18,8 +18,23 @@ class ScaleflexFilerobot extends Plugin
         parent::install($installContext);
 
         $config = $this->container->get('Shopware\Core\System\SystemConfig\SystemConfigService');
+        $context = Context::createDefaultContext();
+
+        /**
+         * Create media folder "Filerobot DAM"
+         */
+        $mediaFolderRepository = $this->container->get('media_folder.repository');
+        $criteriaFR = new Criteria();
+        $criteriaFR->addFilter(new EqualsFilter('name', 'Filerobot DAM'));
+        $mediaFolder = $mediaFolderRepository->search($criteriaFR, $context)->first();
+        if ($mediaFolder) {
+            $folderId = $mediaFolder->id;
+        } else {
+            $folderId = $this->createMediaFolderWithConfiguration($context);
+        }
 
         //set the specified values as defaults
+        $config->set('ScaleflexFilerobot.config.frFolderId', $folderId);
         $config->set('ScaleflexFilerobot.config.frActivation', true);
         $config->set('ScaleflexFilerobot.config.frUploadDirectory', "/wp_assets");
     }
@@ -33,89 +48,65 @@ class ScaleflexFilerobot extends Plugin
         /**
          * Delete all related data
          */
-        $connection = \Shopware\Core\Kernel::getConnection();
-        $connection->executeStatement("DROP TABLE IF EXISTS `filerobot_media`");
+        $this->dropFilerobotData();
     }
 
     public function activate(ActivateContext $activateContext): void
     {
         parent::activate($activateContext);
-
-        /**
-         * Add more field url, is_filerobot
-         */
-        $connection = \Shopware\Core\Kernel::getConnection();
-        $query = $connection->executeQuery("SHOW COLUMNS FROM `media` LIKE 'filerobot_url'");
-        $result = $query->fetchAllAssociative();
-        if (count($result) == 0) {
-            $connection->executeStatement("ALTER TABLE `media` 
-            ADD COLUMN `filerobot_url` VARCHAR(255) NULL AFTER `updated_at`;");
-        }
-
-        $query = $connection->executeQuery("SHOW COLUMNS FROM `media` LIKE 'is_filerobot'");
-        $result = $query->fetchAllAssociative();
-        if (count($result) == 0) {
-            $connection->executeStatement("ALTER TABLE `media` 
-            ADD COLUMN `is_filerobot` TINYINT(1) NULL AFTER `filerobot_url`;");
-        }
-
-        $query = $connection->executeQuery("SHOW COLUMNS FROM `media` LIKE 'filerobot_uuid'");
-        $result = $query->fetchAllAssociative();
-        if (count($result) == 0) {
-            $connection->executeStatement("ALTER TABLE `media` 
-            ADD COLUMN `filerobot_uuid` VARCHAR(255) NULL AFTER `is_filerobot`;");
-        }
-
-        $query = $connection->executeQuery("SELECT HEX(id) FROM `media_folder` WHERE `name` = 'Filerobot DAM'");
-        $result = $query->fetchOne();
-        if ($result) {
-            $folderId = strtolower($result);
-        } else {
-            $folderId = $this->createMediaFolderWithConfiguration();
-        }
-        $config = $this->container->get('Shopware\Core\System\SystemConfig\SystemConfigService');
-        $config->set('ScaleflexFilerobot.config.frFolderId', $folderId);
     }
 
     /**
      * Remove Filerobot related columns in media table
-     * @param $connection
      * @return void
      */
-    private function dropFilerobotColumns($connection): void
+    private function dropFilerobotData(): void
     {
-        $query = $connection->executeQuery("SELECT HEX(id) as id
-                    FROM media 
-                    WHERE is_filerobot = 1");
-        $media = $query->fetchAllAssociative();
-        foreach ($media as $item) {
-            $media_id = $item['id'];
-            $connection->executeStatement('DELETE FROM `media_tag` where media_id = UNHEX("' . $media_id . '")');
-            $connection->executeStatement('DELETE FROM `media_thumbnail` where media_id = UNHEX("' . $media_id . '")');
-            $connection->executeStatement('DELETE FROM `media_translation` where media_id = UNHEX("' . $media_id . '")');
-            $connection->executeStatement('DELETE FROM `product_media` where media_id = UNHEX("' . $media_id . '")');
-            $connection->executeStatement('DELETE FROM `theme_media` where media_id = UNHEX("' . $media_id . '")');
-            $connection->executeStatement('UPDATE `user` SET avatar_id = NULL where avatar_id = UNHEX("' . $media_id . '")');
-            $connection->executeStatement('UPDATE `category` SET media_id = NULL where media_id = UNHEX("' . $media_id . '")');
-            $connection->executeStatement('UPDATE `mail_template_media` SET media_id = NULL where media_id = UNHEX("' . $media_id . '")');
-        }
-        $connection->executeStatement('DELETE FROM `media` where `is_filerobot` = 1');
-        $connection->executeStatement("ALTER TABLE `media` DROP COLUMN `is_filerobot`,
-        DROP COLUMN `filerobot_url`,
-        DROP COLUMN `filerobot_uuid`;");
+        $context = Context::createDefaultContext();
 
-        //remove folder "Filerobot"
-        $connection->executeStatement("DELETE FROM `media_default_folder` where `entity` = 'filerobot'");
-        $connection->executeStatement("DELETE FROM `media_folder` where `name` = 'Filerobot DAM'");
+        /**
+         * Delete medias
+         */
+        $query = $this->container
+            ->get('Doctrine\DBAL\Connection')
+            ->executeQuery("SELECT HEX(id) as id, HEX(media_id) as media_id FROM filerobot_media");
+        $filerobotMedias = $query->fetchAllAssociative();
+        if ($filerobotMedias) {
+            $mediaRepository = $this->container->get('media.repository');
+            foreach ($filerobotMedias as $item) {
+                $media_id = strtolower($item['media_id']);
+                $mediaRepository->delete([['id' => $media_id]], $context);
+            }
+        }
+
+        /**
+         * Delete media folder
+         */
+        $mediaFolderIds = [];
+        $mediaFolderRepository = $this->container->get('media_folder.repository');
+        $criteriaFR = new Criteria();
+        $criteriaFR->addFilter(new EqualsFilter('name', 'Filerobot DAM'));
+        $mediaFolder = $mediaFolderRepository->search($criteriaFR, $context)->first();
+        if ($mediaFolder) {
+            $mediaFolderIds[] = ['id' => $mediaFolder->id];
+        }
+        $mediaFolderRepository->delete($mediaFolderIds, $context);
+
+        /**
+         * Drop table `filerobot_media`
+         */
+        $this->container
+            ->get('Doctrine\DBAL\Connection')
+            ->executeStatement("DROP TABLE IF EXISTS `filerobot_media`");
     }
 
     /**
      * Create new folder "Filerobot DAM"
+     * @param Context $context
      * @return string
      */
-    private function createMediaFolderWithConfiguration(): string
+    private function createMediaFolderWithConfiguration(Context $context): string
     {
-        $context = Context::createDefaultContext();
         $mediaFolderRepository = $this->container->get('media_folder.repository');
         $folderId = Uuid::randomHex();
         $mediaFolderRepository->upsert([
